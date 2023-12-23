64e3890c8d4defd50c6eb1efc88078db38788537
inukaki <inukaki8@gmail.com>
2023-12-23 16:46:16 +0900

    毎週日曜日21時に発火するイベントを追加

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 const registerFile = require('./commands/register.js')
 // discord.jsから必要な機能を取得
 const { Client, GatewayIntentBits } = require('discord.js');
+
+const cron = require('node-cron');
+
 // config.jsonからトークンを取得
 const { token } = require('./config.json');
 const unregisterFile = require('./commands/unregister.js');
@@ -18,6 +21,18 @@ const client = new Client(
 // botを起動したときに実行されるイベント
 client.on('ready', () => {
     console.log('ボットが起動したよ');
+
+    // チャンネルIDを指定
+    const channelId = '1185099222257762397';
+    const channel = client.channels.cache.get(channelId);  
+
+    // 毎週日曜日の21時に実行されるイベント
+    // スケジュール表現は左から、分、時、日、月、曜日に対応している
+    cron.schedule('* * 21 * * 0', async () => {
+      channel.send('毎週日曜日21時にメッセージを送信するよ♪');
+      console.log('毎週日曜日21時にメッセージを送信するよ♪');
+    }
+    );
   });
 
 // コマンドが実行されたときに実行されるイベント
@@ -49,6 +64,6 @@ client.on('interactionCreate', async (interaction) => {
     }
   });  
 
-
+  
 client.login(token);
   
