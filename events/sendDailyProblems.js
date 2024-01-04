2b25f66418663aa0fc82982b42d707226c86ddc2
inukaki <inukaki8@gmail.com>
2024-01-04 21:55:47 +0900

    今日の一問機能を作成（messageIDは固定）

--- events/sendDailyProblems.js
+++ events/sendDailyProblems.js
@@ -1,6 +1,6 @@
 const cron = require('node-cron');
 const { send } = require('process');
-const { axios } = require('axios');
+const axios = require('axios');
 const shojinSample = require('../sample/shojinSample.json');
 
 
@@ -11,21 +11,63 @@ module.exports = {
         // botを起動したときに実行されるイベント
         client.on('ready', () => {
             
+            const emojis = ['🩶', '🤎', '💚', '🩵', '💙', '💛', '🧡', '❤️'];
+
+            const emojiToColor = {
+                '🩶': 'Gray',
+                '🤎': 'Brown',
+                '💚': 'Green',
+                '🩵': 'Cyan',
+                '💙': 'Blue',
+                '💛': 'Yellow',
+                '🧡': 'Orange',
+                '❤️': 'Red'
+            };
+            
             // 毎日の7時に実行されるイベント
             // スケジュール表現は左から、秒、分、時、日、月、曜日に対応している
             cron.schedule('0 0 7 * * *', async () => {
+            // cron.schedule('0 * * * * *', async () => {  // テスト用
 
-                // 現在の時刻のミリ秒を所得。基準は2023/12/23 21:00 = 1703332800
                 
-
                 client.channels.cache.filter(ch => ch.name === '今日の一問').forEach(async (ch) => { 
-                    
-                });
+                    // 今日の一問を所得
+                    const dailyProblem = await getDailyProblem();
+                    console.log(dailyProblem.Gray.name);
 
-                // message.guild.channels.cache.find(ch => ch.name === '精進通知')?.send('ここは精進通知チャンネルです。');
-                // channel.send('毎週月曜日21時にメッセージを送信するよ♪');
-                // console.log('毎週月曜日21時にメッセージを送信するよ♪');
+                    var text ="本日の「今日の一問」はこちらです！\n";
+    
+                    const messageId = "1192398645094514821";
+                    // リアクションの付与状態を所得
+                    const message = await ch.messages.fetch(messageId);
+                    if(!message){
+                        console.log('メッセージが見つかりませんでした');
+                        return;
+                    }
+                    for(const emoji of emojis) {
+                        const reaction = message.reactions.cache.get(emoji);
+                        const color = emojiToColor[emoji];
+                        if (reaction.count > 1) {
+                            const contestId = dailyProblem[color].contestID;
+                            const problemId = dailyProblem[color].problemID; 
+                            text += `${emoji}:https://atcoder.jp/contests/${contestId}/tasks/${problemId}\n`
+                        }
+                    }
+                    console.log(text);
+                    ch.send(text);
+                });
             });
         });
+    }
+}
+// 今日の一問を所得する関数
+async function getDailyProblem() {
+    const url = 'http://api:3000/api/daily';
+    try {
+        const response = await axios.get(url);
+        const dailyProblem = response.data;
+        return dailyProblem;
+    } catch (error) {
+        console.error(`error in getDailyProblem: ${error}`);
     }
 }
